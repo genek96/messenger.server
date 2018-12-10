8b239192f7b0342e999a96f79677dfcd0923ae41
Evgeniy <genek_96@mail.ru>
2018-12-10 17:45:52 +0300

    Plugin loader created

---
+++ src/main/java/plugins/JarLoader.java
@@ -0,0 +1,41 @@
+package plugins;
+
+import java.io.File;
+import java.net.*;
+import java.nio.file.Paths;
+
+public class JarLoader {
+    private final String classPath;
+
+    public JarLoader(String classPath) {
+        this.classPath = classPath;
+    }
+
+    public Class loadClass(String className, String classPackage) throws ClassNotFoundException {
+        URL url = getUrlToResource(className);
+        URL[] urls = new URL[] {url};
+        ClassLoader loader = new URLClassLoader(urls);
+        classPackage = (classPackage == null) ? "" : classPackage;
+        return loader.loadClass(classPackage+className);
+    }
+
+    private File findFile(String fileName) throws Throwable {
+        ClassLoader loader = getClass().getClassLoader();
+        URL url = loader.getResource("Factorial.jar");
+
+//        File file = new File(classPath + fileName+".jar");
+        File file = new File(url.getPath());
+        System.out.println(file.getAbsolutePath());
+        if (file.exists()){
+            return file;
+        } else {
+            return null;
+        }
+    }
+
+    private URL getUrlToResource(String fileName) {
+        ClassLoader loader = getClass().getClassLoader();
+        URL url = loader.getResource(classPath+fileName+".jar");
+        return url;
+    }
+}
---
+++ src/main/java/plugins/PluginLoader.java
@@ -0,0 +1,98 @@
+package plugins;
+
+import org.apache.log4j.Logger;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
+
+public class PluginLoader extends ClassLoader {
+
+    private static final Logger log = Logger.getLogger(PluginLoader.class);
+
+    private Map classesHash = new HashMap();
+    public final String[] classPath;
+
+    public PluginLoader(String[] classPath){
+        this.classPath = classPath;
+    }
+
+    @Override
+    protected synchronized Class loadClass (String name, boolean resolve) throws ClassNotFoundException {
+        Class result = findClass(name);
+        if (resolve){
+            resolveClass(result);
+        }
+        return result;
+    }
+
+    @Override
+    protected Class findClass(String name) throws ClassNotFoundException {
+        Class result = (Class) classesHash.get(name);
+        if (result != null){
+            return result;
+        }
+        File file = findFile(name.replace('.','/'), ".class");
+        if (file == null){
+            return findSystemClass(name);
+        }
+        try {
+            byte[] classBytes = loadFileAsBytes(file);
+            result = defineClass(name, classBytes, 0, classBytes.length);
+        } catch (IOException e){
+            throw new ClassNotFoundException("Cannot load class "+name+" : "+e);
+        } catch (ClassFormatError e){
+            throw new ClassNotFoundException("Format of class file incorrect for class "+
+                    name+" : "+e);
+        }
+        classesHash.put(name, result);
+        return result;
+
+    }
+
+    protected URL findResource(String name){
+        File file = findFile(name, "");
+        if (file == null){
+            return null;
+        }
+        try {
+            return file.toURI().toURL();
+        } catch (MalformedURLException e) {
+            return null;
+        }
+    }
+
+    private byte[] loadFileAsBytes(File file) throws IOException {
+        byte[] result = new byte[(int)file.length()];
+        FileInputStream fileStream = new FileInputStream(file);
+        try {
+            fileStream.read(result, 0, result.length);
+        } finally {
+            try {
+                fileStream.close();
+            } catch (IOException e) {
+                log.error(e.getMessage()+" : "+ Arrays.toString(e.getStackTrace()));
+            }
+        }
+        return result;
+    }
+
+    private File findFile(String name, String extension) {
+        for (int i = 0; i < classPath.length; i++){
+            File file = new File((new File(classPath[i])).getPath()+
+                    File.separatorChar+name.replace('/',File.separatorChar)+extension);
+            if (file.exists()){
+                return file;
+            }
+        }
+        return null;
+    }
+}
