dbec30ed8fe021ac3de44f195944a2b2dcef85ad
Evgeniy <genek_96@mail.ru>
2018-12-11 16:33:21 +0300

    Added loading properties from the config file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
+import java.net.URL;
+import java.util.Properties;
+
 public class Main {
     public static void main (String[] args) throws Throwable{
 
-        final int port = 12345;
+        //Loading configurations
+        Properties properties = new Properties();
+        URL url = ClassLoader.getSystemClassLoader().getResource("config.properties");
+        properties.load(url.openStream());
+        final int port = Integer.parseInt(properties.getProperty("port"));
+        //Setting up objects
         ClientContainer container = new ClientContainer();
         (new Thread(new Connector(port, container))).start();
 
